c73c1b58a9d927a9531382e3db38f2e32edc1d19
Shrikanth <Shriaknthgautham@gmail.com>
2025-05-19 20:52:36 +0530

    Initial commit

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -55,7 +55,7 @@ const testimonials = [
   },
 ];
 
-export function TestimonialCarousel() {
+export default function TestimonialCarousel() {
   return (
     <div className="w-full max-w-[1000px] mx-auto mt-10 mb-10 py-10">
       <h2 className="text-2xl font-medium text-center mb-10 font-poppins">
